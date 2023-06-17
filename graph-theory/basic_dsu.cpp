560b1655bd8037adf8f807225385d0d7dbeca561
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2023-06-17 15:31:42 +0800

    Update basic_dsu.cpp

--- graph-theory/basic_dsu.cpp
+++ graph-theory/basic_dsu.cpp
@@ -1,18 +1,23 @@
 struct dsu {
-  vector<int> dsu;
+  vector<int> dsu,sizes;
   public:
   void resize(int n) {
+    sizes.resize(n + 5);
     dsu.resize(n + 5);
-    for(int i=0; i<n+5; i++) dsu[i] = i;
+    for(int i=0; i<n+5; i++) dsu[i] = i, sizes[i] = 1;
   }
   int set_of(int u) {
     if(dsu[u] == u) return u;
     return dsu[u] = set_of(dsu[u]);
   }
   void union_(int u, int v) {
+    int su = set_of(u), sv = set_of(v);
+    if(su == sv) return;
+    int ss = sizes[su] + sizes[sv];
     dsu[set_of(u)] = set_of(v);
+    sizes[set_of(u)] = ss;
   }
   void merge(int u, int v) {
-    dsu[set_of(u)] = set_of(v);
+    union_(u, v);
   }
 };
