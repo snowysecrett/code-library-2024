7d20ae8610345c959c361dfaf125cc68ef697499
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2023-07-09 22:23:46 +0800

    Create suffix_array.cpp

---
+++ strings/suffix_array.cpp
@@ -0,0 +1,55 @@
+vector<int> suffix_array(string s) {
+  int n = s.size();
+  vector<pair<int, pair<int, int> > > v; 
+  for(int i=0; i<n; i++) v.push_back({i, {0, s[i]}});
+  sort(v.begin(),v.end(),equivalence);
+  int mm = max(128ll, n);
+  for(int i=0; i<19; i++) {
+    for(int j=0; j<n; j++) {
+      v[j].second.first = v[j].second.second;
+    }
+    for(int j=0; j<n; j++) {
+      pos[v[j].first] = j;
+    }
+    for(int j=0; j<n; j++) {
+      int tar = v[j].first + (1 << i);
+      tar %= n;
+      v[j].second.second = v[pos[tar]].second.first;
+    }
+    for(int j=0; j<mm; j++) wow[j].clear();
+    for(int j=0; j<n; j++) wow[v[j].second.second].push_back({v[j].first,v[j].second.first});
+    int ptr=0;
+    for(int j=0; j<mm; j++) {
+      for(pair<int,int> x: wow[j]) {
+        v[ptr].first = x.first;
+        v[ptr].second.first = x.second;
+        v[ptr].second.second = j;
+        ptr++;
+      }
+    }
+    for(int j=0; j<mm; j++) wow[j].clear();
+    for(int j=0; j<n; j++) wow[v[j].second.first].push_back({v[j].first,v[j].second.second});
+    ptr=0;
+    for(int j=0; j<mm; j++) {
+      for(pair<int,int> x: wow[j]) {
+        v[ptr].first = x.first;
+        v[ptr].second.first = j;
+        v[ptr].second.second = x.second;
+        ptr++;
+      }
+    }
+    vector<int> newClasses;
+    newClasses.push_back(0);
+    for(int j=1; j<n; j++) {
+      if(v[j].second != v[j-1].second) newClasses.push_back(newClasses[j-1] + 1);
+      else newClasses.push_back(newClasses[j-1]);
+    }
+    for(int j=0; j<n; j++) {
+      v[j].second.first = 0;
+      v[j].second.second = newClasses[j];
+    }
+  }
+  vector<int> w;
+  for(int i=0; i<n; i++) w.push_back(v[i].first);
+  return w;
+}
