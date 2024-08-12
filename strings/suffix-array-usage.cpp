59be49ad7fe55553bbaf5244615f5a853c8c1691
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2024-08-13 04:01:14 +0800

    Update suffix-array-usage.cpp

--- strings/suffix-array-usage.cpp
+++ strings/suffix-array-usage.cpp
@@ -1,19 +1,17 @@
-#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
 const int MAXN = 1e6 + 10;
+int pos[MAXN];
+int newClasses[MAXN];
+pair<int, pair<int, int> > v[MAXN], w[MAXN];
 bool equivalence(pair<int,pair<int,int> > x, pair<int,pair<int,int> > y) {
   return x.second.second<y.second.second;
 }
-int pos[MAXN];
-int newClasses[MAXN];
-int ps[MAXN];
-pair<int, pair<int, int> > v[MAXN], w[MAXN]; 
+int ps[1024];
 vector<int> suffix_array(string s) {
   int n = s.size();
   for(int i=0; i<n; i++) v[i] = {i, {0, s[i]}};
   sort(v, v + n, equivalence);
-  int mm = max(128, n);
   for(int i=0; i<20; i++) {
     for(int j=0; j<n; j++) {
       v[j].second.first = v[j].second.second;
@@ -21,26 +19,40 @@ vector<int> suffix_array(string s) {
     for(int j=0; j<n; j++) {
       pos[v[j].first] = j;
     }
-    for(int j=0; j<mm; j++) ps[j] = 0;
+    //.second
+    for(int j=0; j<1024; j++) ps[j] = 0;
     for(int j=0; j<n; j++) {
       int tar = v[j].first + (1 << i);
       tar %= n;
       v[j].second.second = v[pos[tar]].second.first;
-      ps[v[j].second.second]++;
+      ps[v[j].second.second & 1023]++;
     }
-    for(int j=1; j<mm; j++) ps[j] += ps[j-1];
+    for(int j=1; j<1024; j++) ps[j] += ps[j-1];
     for(int j=n-1; j>=0; j--) {
-      int ec = v[j].second.second;
-      ps[ec]--;
-      w[ps[ec]] = v[j];
+      ps[v[j].second.second & 1023]--;
+      w[ps[v[j].second.second & 1023]] = v[j];
     }
-    for(int j=0; j<mm; j++) ps[j] = 0;
-    for(int j=0; j<n; j++) ps[w[j].second.first]++;
-    for(int j=1; j<mm; j++) ps[j] += ps[j-1];
+    for(int j=0; j<1024; j++) ps[j] = 0;
+    for(int j=0; j<n; j++) ps[w[j].second.second >> 10]++;
+    for(int j=1; j<1024; j++) ps[j] += ps[j-1];
     for(int j=n-1; j>=0; j--) {
-      int ec = w[j].second.first;
-      ps[ec]--;
-      v[ps[ec]] = w[j];
+      ps[w[j].second.second >> 10]--;
+      v[ps[w[j].second.second >> 10]] = w[j];
+    }
+    //.first
+    for(int j=0; j<1024; j++) ps[j] = 0;
+    for(int j=0; j<n; j++) ps[v[j].second.first & 1023]++;
+    for(int j=1; j<1024; j++) ps[j] += ps[j-1];
+    for(int j=n-1; j>=0; j--) {
+      ps[v[j].second.first & 1023]--;
+      w[ps[v[j].second.first & 1023]] = v[j];
+    }
+    for(int j=0; j<1024; j++) ps[j] = 0;
+    for(int j=0; j<n; j++) ps[w[j].second.first >> 10]++;
+    for(int j=1; j<1024; j++) ps[j] += ps[j-1];
+    for(int j=n-1; j>=0; j--) {
+      ps[w[j].second.first >> 10]--;
+      v[ps[w[j].second.first >> 10]] = w[j];
     }
     newClasses[0] = 0;
     for(int j=1; j<n; j++) {
@@ -58,11 +70,11 @@ vector<int> suffix_array(string s) {
 }
 void solve(int tc) {
   string s;
-  int len = 500000;
+  /*int len = 500000;
   for(int i=0; i<len; i++) {
     s += (rand() % 2 ? 'a' + rand() % 26 : 'A' + rand() % 26);
-  }
-  //cin >> s;
+  }*/
+  cin >> s;
   s = s + "$";
   vector<int> suf = suffix_array(s);
 
