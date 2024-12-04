88afe5cc52a615425698427ec0482c91cc5fab03
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2024-12-04 09:02:31 +0800

    Update fraction.cpp

--- misc/fraction.cpp
+++ misc/fraction.cpp
@@ -1,28 +1,31 @@
 struct Fraction {
-  __int128 gcdd(__int128 x, __int128 y) {
-    if(x == 0) return y;
-    if(y == 0) return x;
-    if(x < y) return gcdd(x, y % x);
-    return gcdd(y, x % y);
-  }
   int p, q;
-  Fraction operator*(Fraction other) {
-    __int128 num = p, den = q;
-    num *= other.p, den *= other.q;
-    __int128 g = gcdd(num, den);
-    num /= g, den /= g;
-    int n1 = num, n2 = den;
-    Fraction product = {n1, n2};
-    return product;
+  Fraction() : p(0), q(1) {}
+  Fraction(int num, int den) {
+    assert(den != 0 && "Denominator cannot be zero");
+    int g = gcd(num, den);
+    p = num / g;
+    q = den / g;
+    if (q < 0) {
+      p = -p;
+      q = -q;
+    }
   }
-  Fraction operator+(Fraction other) {
-    __int128 l = q * other.q / gcdd(q, other.q); 
-    __int128 num = p * l/q + other.p * l/other.q;
-    __int128 g = gcdd(l, num);
-    l /= g, num /= g;
-    int n1 = num, n2 = l;
-    Fraction sum = {n1, n2};
-    return sum;
+  Fraction operator+(Fraction other) const {
+    int l = q * other.q / gcd(q, other.q); 
+    int num = p * l/q + other.p * l/other.q;
+    return Fraction(num, l);
+  }
+  Fraction operator-(Fraction other) const {
+    int l = q * other.q / gcd(q, other.q); 
+    int num = p * l/q - other.p * l/other.q;
+    return Fraction(num, l);
+  }
+  Fraction operator*(Fraction other) const {
+    return Fraction(p * other.p, q * other.q);
+  }
+  Fraction operator/(Fraction other) const {
+    return Fraction(p * other.q, q * other.p);
   }
 };
 Fraction frac(int p, int q) {
